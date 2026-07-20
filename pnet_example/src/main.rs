e1e8c74988d8921528a90d0481585dbaa5977621
nosaemma21 <emmanuelnosakhare052@gmail.com>
2026-07-20 15:31:50 +0100

    feat(pnet_lesson): add pnet lesson

---
+++ pnet_example/src/main.rs
@@ -0,0 +1,71 @@
+extern crate pnet;
+
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::{self, NetworkInterface};
+use pnet::packet::Packet;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::tcp::TcpPacket;
+
+use core::panic;
+use std::env;
+
+fn handle_packet(ethernet: &EthernetPacket) {
+    match ethernet.get_ethertype() {
+        EtherTypes::Ipv4 => {
+            let header = Ipv4Packet::new(ethernet.payload());
+            if let Some(header) = header {
+                match header.get_next_level_protocol() {
+                    IpNextHeaderProtocols::Tcp => {
+                        let tcp = TcpPacket::new(header.payload());
+                        if let Some(tcp) = tcp {
+                            println!(
+                                "Got a TCP packet {}: {} to {}:{}",
+                                header.get_source(),
+                                tcp.get_source(),
+                                header.get_destination(),
+                                tcp.get_destination()
+                            )
+                        }
+                    }
+                    _ => println!("Ignoring non TCP packet"),
+                }
+            }
+        }
+        _ => println!("Ignonring non Ipv4 packet"),
+    }
+}
+
+fn main() {
+    let interface_name = env::args().nth(1).unwrap();
+
+    let interface = datalink::interfaces()
+        .into_iter()
+        .filter(|iface: &NetworkInterface| iface.name == interface_name)
+        .next()
+        .expect("Error getting interface");
+
+    let (_tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => {
+            panic!(
+                "An error occured when creating the datalink channels: {}",
+                e
+            )
+        }
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                let packet = EthernetPacket::new(packet).unwrap();
+                handle_packet(&packet);
+            }
+            Err(e) => {
+                panic!("An error occured while reading: {}", e);
+            }
+        }
+    }
+}
